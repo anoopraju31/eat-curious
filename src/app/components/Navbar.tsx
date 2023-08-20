dcf8ffb1a3c17771eab37582b152c82865851d9f
anoopraju31 <anoopraju31@gmail.com>
2023-08-20 12:51:06 +0530

    Navbar component: menu added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,9 +15,9 @@ const NavLink = ({ title, link }: NavLinkProps) => (
 const Navbar = () => {
 	return (
 		<nav>
-			<div className='sm:h-[var(--nav-height-sm)] lg:h-[var(--nav-height-lg)] px-[var(--nav-padding-x)] sm:px-[var(--nav-padding-x-sm)] lg:px-[var(--nav-padding-x-lg)] flex justify-between items-center font-text-font font-medium bg-black text-[var(--white)]'>
+			<div className='w-full sm:h-[var(--nav-height-sm)] lg:h-[var(--nav-height-lg)] px-[var(--nav-padding-x)] sm:px-[var(--nav-padding-x-sm)] lg:px-[var(--nav-padding-x-lg)] pt-4 sm:pt-0 static sm:absolute top-4 sm:top-0 bg-[var(--black)] flex justify-center md:justify-between items-center font-text-font font-medium text-[var(--white)]'>
 				{/* Navigation desktop - left */}
-				<ul className='mr-10 lg:mr-14 flex-1 flex items-center uppercase '>
+				<ul className='flex-1 mr-10 lg:mr-14 hidden md:flex items-center uppercase'>
 					<li className='inline-block mr-auto'>
 						<Link href='/#about'> About </Link>
 					</li>
@@ -27,11 +27,11 @@ const Navbar = () => {
 					</li>
 				</ul>
 				{/* Logo */}
-				<Link href='/' className='w-[181px] lg:w-[184px] mt-4'>
+				<Link href='/' className='w-[113px] md:w-[181px] lg:w-[184px] md:mt-4'>
 					<Logo />
 				</Link>
 				{/* Navigation desktop - right */}
-				<ul className='ml-10 lg:ml-14 flex-1 flex items-center uppercase '>
+				<ul className='flex-1 ml-10 lg:ml-14 hidden md:flex items-center uppercase'>
 					<li className='inline-block m-auto'>
 						<Link href='/#about'> Recipes </Link>
 					</li>
@@ -41,6 +41,11 @@ const Navbar = () => {
 					</li>
 				</ul>
 				{/* Menu */}
+				<button className='w-8 h-8 px-[3px] bg-[var(--pink)] md:hidden flex flex-col justify-center  gap-[3px] rounded-md fixed right-6'>
+					<div className='w-full h-[2px] bg-[var(--black)]'></div>
+					<div className='w-full h-[2px] bg-[var(--black)]'></div>
+					<div className='w-full h-[2px] bg-[var(--black)]'></div>
+				</button>
 				{/* navigation mobile */}
 			</div>
 		</nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-	return <main></main>
+	return <main className='h-[200vh] bg-[var(--black)]'></main>
 }
