36dc25226930bd561ec280798dd5c01699500806
anoopraju31 <anoopraju31@gmail.com>
2023-08-25 03:35:34 +0530

    Food Tiles Section: drooling heading added

--- src/app/(home)/sections/Foods.tsx
+++ src/app/(home)/sections/Foods.tsx
@@ -1,7 +1,14 @@
+import { Drooling } from '@/app/components'
 import React from 'react'
 
 const Foods = () => {
-	return <div>Foods</div>
+	return (
+		<section className='w-full px-[var(--padding-x)] md:px-[var(--padding-x-sm)] xl:px-[var(--padding-x-lg)] pt-10 lg:pt-[60px] xl:pt-[100px] z-10 relative overflow-hidden before:content-[""] before:block before:absolute before:w-[150vw] before:left-1/2 before:top-0 before:h-0 before:pb-[138%] before:-translate-x-1/2 before:bg-[var(--black)] before:rounded-[150%] before:z-0 after:content-[""] after:block after:absolute after:top-[20%] lg:after:top-[40%] after:bottom-0 after:left-0 after:right-0 after:bg-[var(--black)] after:-z-10 '>
+			<div className=''>
+				<Drooling />
+			</div>
+		</section>
+	)
 }
 
 export default Foods
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { BrandsCarousel, SpinnerSticker, Ticker } from './components'
 
 export default function Home() {
 	return (
-		<div className='h-[2000vh] bg-[var(--black)] '>
+		<div className='h-[2000vh] bg-[var(--white)] '>
 			<Hero />
 			<main className='w-full mx-auto'>
 				<Ticker />
