926d31021d969bb6ab1324bbc699b8742d7d52d8
anoopraju31 <anoopraju31@gmail.com>
2023-08-26 13:00:10 +0530

    Footer Component: initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Navbar } from './components'
+import { Footer, Navbar } from './components'
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -20,6 +20,7 @@ export default function RootLayout({
 			<body>
 				<Navbar />
 				{children}
+				<Footer />
 			</body>
 		</html>
 	)
