1903f8fa6486349892247391fd2fe6c5e18898a5
anoopraju31 <anoopraju31@gmail.com>
2023-08-26 12:57:51 +0530

    Intrigued Section: styled

--- src/app/(home)/sections/Intrigued.tsx
+++ src/app/(home)/sections/Intrigued.tsx
@@ -1,7 +1,30 @@
+import { Button } from '@/app/components'
+import Image from 'next/image'
 import React from 'react'
 
 const Intrigued = () => {
-	return <div>Intrigued</div>
+	return (
+		<section className='w-full sticky top-0 bg-[var(--white)] text-center text-[var(--black)]'>
+			<div className='overflow-hidden relative  px-[var(--padding-x)] md:px-[var(--padding-x-sm)] xl:px-[var(--padding-x-lg)] py-[var(--padding-y)] md:py-[var(--padding-y-sm)] lg:py-[var(--padding-y-lg)] '>
+				<div className='relative z-10 mx-auto max-w-[var(--content-width)] md:max-w-[var(--content-width-md)] lg:max-w-[var(--content-width-lg)] 1600:max-w-[var(--content-width-3xl)] '>
+					<h3 className='mt-0 mb-[0.7em] font-heading-narrow font-extrabold leading-[0.85em] -tracking-[0.01em] uppercase text-[48px] md:text-[88px] xl:text-[96px]'>
+						Intrigued?
+					</h3>
+					<p className='mx-auto mb-[3em] max-w-[440px] '>
+						<Button title="Let's chat" link='/contact' />
+					</p>
+				</div>
+
+				<Image
+					src='/images/home-6.svg'
+					alt=''
+					width={622}
+					height={619}
+					className='blur-0 md:max-w-[50vh] 1600:max-w-[758px] mx-auto -mt-[100px] relative z-0'
+				/>
+			</div>
+		</section>
+	)
 }
 
 export default Intrigued
