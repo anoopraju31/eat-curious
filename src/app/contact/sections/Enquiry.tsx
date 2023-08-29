816bf2785a5acdaf694cb0f46ca3759cdff8aceb
anoopraju31 <anoopraju31@gmail.com>
2023-08-30 01:21:23 +0530

    Contact Page: Enquiry Section - animated

--- src/app/contact/sections/Enquiry.tsx
+++ src/app/contact/sections/Enquiry.tsx
@@ -1,11 +1,18 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import { pop } from '@/utils/animations'
+import { motion } from 'framer-motion'
 
 const Enquiry = () => {
 	return (
 		<section className='w-full py-[var(--padding-y)] md:py-[var(--padding-y-sm)] lg:py-[var(--padding-y-lg)] px-[var(--padding-x)] md:px-[var(--padding-x-sm)] xl:px-[var(--padding-x-lg)] relative bg-[var(--black)] lg:flex lg:justify-between lg:flex-wrap lg:items-start '>
 			{/* Contact Info */}
-			<div className='relative text-center lg:text-left text-[var(--white)] lg:w-[393px] '>
+			<motion.div
+				variants={pop}
+				initial='initial'
+				whileInView='animate'
+				viewport={{ once: true }}
+				className='relative text-center lg:text-left text-[var(--white)] lg:w-[393px] '>
 				<h4 className='mb-[0.7em] -tracking-[0.01em] text-[40px] md:text-[76px] lg:text-[5vw] font-heading-narrow font-extrabold leading-[0.85em] uppercase '>
 					{' '}
 					General Enquiry{' '}
@@ -31,10 +38,15 @@ const Enquiry = () => {
 				<p className='mb-5 text-base md:text-lg'>NN7 2BA</p>
 
 				<div className='hidden lg:block w-[52px] h-[76px] bg-squiggle3 z-20 bg-no-repeat bg-center bg-contain pointer-events-none absolute top-[130%] left-[30%] -scale-x-100  '></div>
-			</div>
+			</motion.div>
 
 			{/* Contact Form */}
-			<div className='mt-20 mb-[50px] lg:my-0 lg:w-1/2 lg:order-2'>
+			<motion.div
+				variants={pop}
+				initial='initial'
+				whileInView='animate'
+				viewport={{ once: true }}
+				className='mt-20 mb-[50px] lg:my-0 lg:w-1/2 lg:order-2'>
 				<form className=''>
 					<div>
 						{/* Name */}
@@ -140,7 +152,7 @@ const Enquiry = () => {
 						</button>
 					</div>
 				</form>
-			</div>
+			</motion.div>
 		</section>
 	)
 }
