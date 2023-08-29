d26a6680e008424da30208262106041562c73a09
anoopraju31 <anoopraju31@gmail.com>
2023-08-29 15:35:26 +0530

    Contact Page: Enquiry Section - initial

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,13 @@
-import { Hero } from './sections'
+import { Enquiry, Hero } from './sections'
 
 export default function ContactPage() {
 	return (
 		<div className='h-[300vh] '>
 			<Hero />
+
+			<main className='w-full mx-auto'>
+				<Enquiry />
+			</main>
 		</div>
 	)
 }
---
+++ src/app/contact/sections/Enquiry.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Enquiry = () => {
+	return <div>Enquiry</div>
+}
+
+export default Enquiry
