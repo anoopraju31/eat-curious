9e459d7d352d2467d1e7596a5fa7199fea829d31
anoopraju31 <anoopraju31@gmail.com>
2023-08-29 19:47:33 +0530

    Accordion Component- initial

--- src/app/contact/sections/FAQ.tsx
+++ src/app/contact/sections/FAQ.tsx
@@ -1,7 +1,14 @@
+import { Accordion } from '@/app/components'
 import React from 'react'
 
 const FAQ = () => {
-	return <div>FAQ</div>
+	return (
+		<section className='w-full mb-[30px] py-[var(--padding-y)] md:py-[var(--padding-y-sm)] lg:py-[var(--padding-y-lg)] relative bg-[var(--black)]'>
+			<Accordion />
+			<Accordion />
+			<Accordion />
+		</section>
+	)
 }
 
 export default FAQ
---
+++ src/utils/faq.ts
@@ -0,0 +1,27 @@
+interface FAQ {
+	id: number
+	question: string
+	answer: string
+}
+
+export const faqs: FAQ[] = [
+	{
+		id: 1,
+		question: 'How do I become a distributor?',
+		answer:
+			'If you are interested in carrying Eat Curious products, please complete the form below, and we’ll be in touch soon.',
+	},
+	{
+		id: 2,
+		question: 'Are Eat Curious products allergen free?',
+		answer:
+			'At Eat Curious we are passionate about creating products full of flavour and accessible to all. We currently have an expanding range of products which are allergen free and we are working hard behind the scenes to remove all allergens from the entire Eat Curious range.',
+	},
+	{
+		id: 3,
+		question:
+			'Eat Curious Products look like meat but are they really meat free?',
+		answer:
+			'Absolutely! All the Eat Curious products are plant-based and vegan friendly!',
+	},
+]
