bb1050d96f5283f33e4c396cfdb5c5236eaf70a4
anoopraju31 <anoopraju31@gmail.com>
2023-08-30 22:01:07 +0530

    Product Item Component: initial

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { ProductItem } from '../components'
 
 const ProductPage = () => {
-	return <div>ProductPage</div>
+	return (
+		<main className=''>
+			<div className='w-screen h-screen py-[80px] md:py-[100px] xl:py-[120px] fixed top-0 left-0 right-0 bottom-0 bg-[var(--black)]  flex flex-col justify-start items-center  '>
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+				<ProductItem />
+			</div>
+		</main>
+	)
 }
 
 export default ProductPage
