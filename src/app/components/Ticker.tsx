452186033f5655c937c71690c999f99502416dc8
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 21:14:23 +0530

    Ticker - refactor

--- src/app/components/Ticker.tsx
+++ src/app/components/Ticker.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-
-type Props = {
+interface TickerTapeItemProps {
 	title: string
 	isLargeSize: boolean
 }
 
-type TickerProps = {
+interface TickerProps {
 	ticks: string[]
 	background: string
 	position?: string
 	isLargeSize?: true
 }
 
-const TickerTapeItem = ({ title, isLargeSize }: Props) => (
-	<span
-		className={`after:content-[''] after:inline-block ${
-			isLargeSize
-				? 'after:w-[0.8em] after:h-[0.8em] after:mx-[20px]'
-				: 'after:w-4 after:h-4 after:mx-6'
-		} after:bg-no-repeat after:bg-center after:bg-flower after:bg-contain after:align-middle`}>
-		{title}
-	</span>
-)
+const TickerTapeItem = (props: TickerTapeItemProps) => {
+	const { title, isLargeSize } = props
 
-const Ticker = ({ ticks, background, position, isLargeSize }: TickerProps) => {
+	return (
+		<span
+			className={`after:content-[''] after:inline-block ${
+				isLargeSize
+					? 'after:w-[0.8em] after:h-[0.8em] after:mx-[20px]'
+					: 'after:w-4 after:h-4 after:mx-6'
+			} after:bg-no-repeat after:bg-center after:bg-flower after:bg-contain after:align-middle`}>
+			{title}
+		</span>
+	)
+}
+
+const Ticker = (props: TickerProps) => {
+	const { ticks, background, position, isLargeSize } = props
+
 	return (
 		<div
 			style={{ backgroundColor: background }}
