fe7b680fd6e8cc884c347abe9d2173a94939ce31
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 14:15:59 +0530

    Enquiry Form - animated

--- src/app/contact/components/EnquiryForm.tsx
+++ src/app/contact/components/EnquiryForm.tsx
@@ -1,62 +1,70 @@
-import React from 'react'
+import { motion } from 'framer-motion'
 import { EnquiryInput, EnquirySelect, EnquiryTextarea } from '.'
 import { enquirySelect } from '@/utils/enquiry'
+import { pop } from '@/utils/animations'
 
 const EnquiryForm = () => {
 	return (
-		<form>
-			{/* Name - input */}
-			<EnquiryInput
-				id='name'
-				label='Name'
-				name='name'
-				placeholder='full name'
-				type='text'
-			/>
+		<motion.div
+			variants={pop}
+			initial='initial'
+			whileInView='animate'
+			viewport={{ once: true }}
+			className='mt-20 mb-[50px] lg:my-0 lg:w-1/2 lg:order-2'>
+			<form>
+				{/* Name - input */}
+				<EnquiryInput
+					id='name'
+					label='Name'
+					name='name'
+					placeholder='full name'
+					type='text'
+				/>
 
-			{/* Email - input */}
-			<EnquiryInput
-				id='email'
-				label='Email'
-				name='email'
-				placeholder='email address'
-				type='email'
-			/>
+				{/* Email - input */}
+				<EnquiryInput
+					id='email'
+					label='Email'
+					name='email'
+					placeholder='email address'
+					type='email'
+				/>
 
-			{/* Contact Number - input */}
-			<EnquiryInput
-				id='contact-number'
-				label='Contact Number'
-				name='contact number'
-				placeholder='contact no.'
-				type='text'
-			/>
+				{/* Contact Number - input */}
+				<EnquiryInput
+					id='contact-number'
+					label='Contact Number'
+					name='contact number'
+					placeholder='contact no.'
+					type='text'
+				/>
 
-			{/* Enquiry Select */}
-			<EnquirySelect
-				id='enquiry-select'
-				label='Enquiry Type'
-				name='enquiry select'
-				options={enquirySelect}
-			/>
+				{/* Enquiry Select */}
+				<EnquirySelect
+					id='enquiry-select'
+					label='Enquiry Type'
+					name='enquiry select'
+					options={enquirySelect}
+				/>
 
-			{/* Message - input */}
-			<EnquiryTextarea
-				id='message'
-				cols={50}
-				label='Message'
-				name='message'
-				placeholder='message'
-				rows={10}
-			/>
+				{/* Message - input */}
+				<EnquiryTextarea
+					id='message'
+					cols={50}
+					label='Message'
+					name='message'
+					placeholder='message'
+					rows={10}
+				/>
 
-			{/* Submit Button */}
-			<div className='-mt-[10.8px]'>
-				<button className='w-auto min-w-[120px] px-[0.7em] pt-[0.2em] pb-[0.6em] rounded text-[20px] md:text-[24px] text-center bg-[var(--pink)] text-[var(--black)] font-heading-narrow tracking-[0.01em] uppercase font-extrabold '>
-					Send Enquiry
-				</button>
-			</div>
-		</form>
+				{/* Submit Button */}
+				<div className='-mt-[10.8px]'>
+					<button className='w-auto min-w-[120px] px-[0.7em] pt-[0.2em] pb-[0.6em] rounded text-[20px] md:text-[24px] text-center bg-[var(--pink)] text-[var(--black)] font-heading-narrow tracking-[0.01em] uppercase font-extrabold '>
+						Send Enquiry
+					</button>
+				</div>
+			</form>
+		</motion.div>
 	)
 }
 
--- src/app/contact/sections/Enquiry.tsx
+++ src/app/contact/sections/Enquiry.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import { pop } from '@/utils/animations'
 import { motion } from 'framer-motion'
+import { EnquiryForm } from '../components'
 
 const Enquiry = () => {
 	return (
@@ -35,109 +36,7 @@ const Enquiry = () => {
 			</motion.div>
 
 			{/* Contact Form */}
-			<motion.div
-				variants={pop}
-				initial='initial'
-				whileInView='animate'
-				viewport={{ once: true }}
-				className='mt-20 mb-[50px] lg:my-0 lg:w-1/2 lg:order-2'>
-				<form className=''>
-					<div>
-						{/* Name */}
-						<div className='mb-4'>
-							<label htmlFor='name' className='sr-only'>
-								Name
-							</label>
-							<input
-								id='name'
-								type='text'
-								placeholder='full name'
-								className='w-full h-auto px-[1.3em] pt-[0.7em] pb-[1em] font-medium tracking-[0.01em] font-heading-narrow text-[18px] text-[var(--white)] text-left leading-[1em] uppercase rounded border-none outline-none shadow-none bg-[rgba(255,246,246,0.1)] placeholder:text-[rgba(4,47,26,0.5)] focus:shadow-inset'
-							/>
-						</div>
-
-						{/* Email */}
-						<div className='mb-4'>
-							<label htmlFor='email' className='sr-only'>
-								Email
-							</label>
-							<input
-								id='email'
-								type='text'
-								placeholder='Email Address'
-								className='w-full h-auto px-[1.3em] pt-[0.7em] pb-[1em] font-medium tracking-[0.01em] font-heading-narrow text-[18px] text-[var(--white)] text-left leading-[1em] uppercase rounded border-none outline-none shadow-none bg-[rgba(255,246,246,0.1)] placeholder:text-[rgba(4,47,26,0.5)] focus:shadow-inset'
-							/>
-						</div>
-
-						{/* Contact */}
-						<div className='mb-4'>
-							<label htmlFor='contact-number' className='sr-only'>
-								Contact Number
-							</label>
-							<input
-								id='contact-number'
-								type='text'
-								placeholder='Contact No.'
-								className='w-full h-auto px-[1.3em] pt-[0.7em] pb-[1em] font-medium tracking-[0.01em] font-heading-narrow text-[18px] text-[var(--white)] text-left leading-[1em] uppercase rounded border-none outline-none shadow-none bg-[rgba(255,246,246,0.1)] placeholder:text-[rgba(4,47,26,0.5)] focus:shadow-inset'
-							/>
-						</div>
-
-						{/* Interest */}
-						<div className='mb-4'>
-							<label htmlFor='interest-in' className='sr-only'>
-								Interest In
-							</label>
-							<select
-								name=''
-								id='interest-in'
-								className='w-full h-auto px-[1.3em] pt-[0.7em] pb-[1em] font-medium tracking-[0.01em] font-heading-narrow text-[18px] text-[var(--white)] text-left leading-[1em] uppercase rounded border-none outline-none shadow-none bg-[rgba(255,246,246,0.1)] placeholder:text-[rgba(4,47,26,0.5)] focus:shadow-inset appearance-none bg-plus bg-no-repeat bg-select relative focus:border-none focus:outline-none'>
-								<option
-									value=''
-									className='text-[rgba(255, 246,246, 0.5)] bg-[var(--black)] border-none outline-none px-[1.3em] pt-[0.7em] pb-[1em]  '>
-									I{"'"}m interested in...
-								</option>
-								<option
-									value=''
-									className='text-[rgba(255, 246,246, 0.5)] bg-[var(--black)] border-none outline-none px-[1.3em] pt-[0.7em] pb-[1em]  '>
-									Becoming a distribution partner
-								</option>
-								<option
-									value=''
-									className='text-[rgba(255, 246,246, 0.5)] bg-[var(--black)] border-none outline-none px-[1.3em] pt-[0.7em] pb-[1em]  '>
-									Stocking an Eat Curious product
-								</option>
-								<option
-									value=''
-									className='text-[rgba(255, 246,246, 0.5)] bg-[var(--black)] border-none outline-none px-[1.3em] pt-[0.7em] pb-[1em]  '>
-									Curious for more...
-								</option>
-							</select>
-						</div>
-
-						{/* Message */}
-						<div className='mb-4'>
-							<label htmlFor='message' className='sr-only'>
-								Message
-							</label>
-							<textarea
-								id='message'
-								placeholder='Message'
-								aria-invalid='false'
-								rows={10}
-								cols={50}
-								className='w-full h-[10em] px-[1.3em] pt-[0.7em] pb-[1.3em] m-0 mb-0 appearance-none font-medium tracking-[0.01em] font-heading-narrow text-[18px] text-[var(--white)] text-left leading-[1.25em] uppercase rounded border-none outline-none shadow-none bg-[rgba(255,246,246,0.1)]  placeholder:text-[rgba(4,47,26,0.5)] focus:shadow-inset'
-							/>
-						</div>
-					</div>
-
-					{/* Submit Button */}
-					<div className='-mt-[10.8px]'>
-						<button className='w-auto min-w-[120px] px-[0.7em] pt-[0.2em] pb-[0.6em] rounded text-[20px] md:text-[24px] text-center bg-[var(--pink)] text-[var(--black)] font-heading-narrow tracking-[0.01em] uppercase font-extrabold '>
-							Send Enquiry
-						</button>
-					</div>
-				</form>
-			</motion.div>
+			<EnquiryForm />
 		</section>
 	)
 }
