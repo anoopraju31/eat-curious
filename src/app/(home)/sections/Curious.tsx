738fd6ddb131ad7cc692e6faf9a8043fcd1aea57
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 12:35:01 +0530

    Curious - refactor

--- src/app/(home)/sections/Curious.tsx
+++ src/app/(home)/sections/Curious.tsx
@@ -1,8 +1,8 @@
 'use client'
+
 import Image from 'next/image'
-import { pop } from '@/utils/animations'
 import { motion } from 'framer-motion'
-import React from 'react'
+import { pop } from '@/utils/animations'
 
 const Curious = () => {
 	return (
