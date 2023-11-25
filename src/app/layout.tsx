a0d83414a0919ecd2692063037fb50010f6c5c64
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 10:55:26 +0530

    Layout - description change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,8 @@ import { Context, Footer, Navbar, SpinnerSticker } from './components'
 
 export const metadata: Metadata = {
 	title: 'Eat Curious | Meat free your mind',
-	description: 'Flavoursome food for everyone​​ Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn't mean flavour free.',
+	description:
+		"Flavoursome food for everyone​​ Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn't mean flavour free.",
 }
 
 export default function RootLayout({
@@ -18,7 +19,6 @@ export default function RootLayout({
 				<link rel='icon' href='/favicon.ico' sizes='any' />
 				<link rel='stylesheet' href='https://use.typekit.net/sqg3ceb.css' />
 				<title> Eat Curious | Meat free your mind </title>
-				
 			</head>
 			<body className='bg-[var(--white)] leading-[1.6em] font-light font-text-font text-lg'>
 				<Context>
