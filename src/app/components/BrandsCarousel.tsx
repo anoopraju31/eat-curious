ee6487f231c7c85845fe1ec870c9410c47cf1bbc
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 12:58:51 +0530

    BrandsCarousel - refactor

--- src/app/components/BrandsCarousel.tsx
+++ src/app/components/BrandsCarousel.tsx
@@ -1,42 +1,38 @@
-import { brands } from '@/utils/carousel'
 import Image from 'next/image'
-import React from 'react'
+import { brands } from '@/utils/carousel'
 
-type Props = {
+type BrandProps = {
 	brand: string
 }
 
-const Brands = ({ brand }: Props) => (
-	<span className='inline-block align-middle mr-20'>
-		<Image
-			src={brand}
-			alt=''
-			width={300}
-			height={200}
-			className='w-auto max-w-[125px] md:max-w-[167px] lg:max-w-[246px] h-auto max-h-[50px] md:max-h-[67px] lg:max-h-[90px]'
-		/>
-	</span>
-)
+const Brand = (props: BrandProps) => {
+	const { brand } = props
+	return (
+		<span className='inline-block align-middle mr-20'>
+			<Image
+				src={brand}
+				alt=''
+				width={300}
+				height={200}
+				className='w-auto max-w-[125px] md:max-w-[167px] lg:max-w-[246px] h-auto max-h-[50px] md:max-h-[67px] lg:max-h-[90px]'
+			/>
+		</span>
+	)
+}
+
+const Brands = () => brands.map((brand) => <Brand key={brand} brand={brand} />)
 
 const BrandsCarousel = () => {
 	return (
 		<div className='relative w-full -mt-[2px] py-[50px] lg:py-[100px] bg-[var(--black)] flex flex-row flex-nowrap z-50 overflow-hidden whitespace-nowrap text-[var(--black)] font-extrabold tracking-[0.01em]'>
 			<div className='inline-block overflow-hidden flex-shrink-0 animate-marquee'>
-				{brands.map((brand) => (
-					<Brands key={brand} brand={brand} />
-				))}
-				{brands.map((brand) => (
-					<Brands key={brand} brand={brand} />
-				))}
+				<Brands />
+				<Brands />
 			</div>
 
 			<div className='inline-block overflow-hidden flex-shrink-0 animate-marquee'>
-				{brands.map((brand) => (
-					<Brands key={brand} brand={brand} />
-				))}
-				{brands.map((brand) => (
-					<Brands key={brand} brand={brand} />
-				))}
+				<Brands />
+				<Brands />
 			</div>
 		</div>
 	)
