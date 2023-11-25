7a14ad9ab9cf78976c0a78c555dc7789c5a71a96
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 12:13:33 +0530

    SpinnerSticker - refactor

--- src/app/components/SpinnerSticker.tsx
+++ src/app/components/SpinnerSticker.tsx
@@ -1,6 +1,6 @@
 'use client'
+
 import { motion, useScroll, useTransform } from 'framer-motion'
-import React from 'react'
 
 const SpinnerSticker = () => {
 	const { scrollY } = useScroll()
@@ -10,7 +10,8 @@ const SpinnerSticker = () => {
 		<motion.div
 			transition={{ type: 'spring', stiffness: 100 }}
 			className='w-[86px] md:w-[129px] h-[86px] md:h-[129px]  fixed bottom-[15%] left-[-20px] md:left-[-30px] z-[999] bg-contain bg-sticker bg-no-repeat bg-center'
-			style={{ rotate }}></motion.div>
+			style={{ rotate }}
+		/>
 	)
 }
 
