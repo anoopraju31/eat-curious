d79d2ad1d37191a9526f9770cd6f17d01440b22d
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 12:50:15 +0530

    Inspiring Section - refactor

--- src/app/(home)/sections/Inspiring.tsx
+++ src/app/(home)/sections/Inspiring.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
-import { pop } from '@/utils/animations'
 import { motion } from 'framer-motion'
+import { pop } from '@/utils/animations'
 
 const Inspiring = () => {
 	return (
