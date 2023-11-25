9104a178849462217dce5663cc78ce6ee6729517
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 21:28:21 +0530

    Root Layout - refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,25 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Context, Footer, Navbar, SpinnerSticker } from './components'
+import { Context, Navbar, SpinnerSticker } from './components'
+import './globals.css'
 
 export const metadata: Metadata = {
 	title: 'Eat Curious | Meat free your mind',
 	description:
-		"Flavoursome food for everyone​​ Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn't mean flavour free.",
+		"Flavoursome food for everyone Our curious creations are made for vegans and meat-eaters alike. Because meat-free doesn't mean flavour free.",
 }
 
-export default function RootLayout({
-	children,
-}: {
+interface RootLayoutProps {
 	children: React.ReactNode
-}) {
+}
+
+export default function RootLayout(props: RootLayoutProps) {
+	const { children } = props
+
 	return (
 		<html lang='en'>
 			<head>
 				<link rel='icon' href='/favicon.ico' sizes='any' />
 				<link rel='stylesheet' href='https://use.typekit.net/sqg3ceb.css' />
-				<title> Eat Curious | Meat free your mind </title>
 			</head>
 			<body className='bg-[var(--white)] leading-[1.6em] font-light font-text-font text-lg'>
 				<Context>
