45c6b4ce2de66ccc2af0ad5a463fd9e8bcb7db40
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 20:44:45 +0530

    Nav Mobile Menu - refactor

--- src/app/components/NavMobileMenu.tsx
+++ src/app/components/NavMobileMenu.tsx
@@ -12,6 +12,13 @@ const NavMobileMenu = (props: NavMobileMenuProps) => {
 	const { closeMenu } = props
 	const router = useRouter()
 
+	const handleClose = (link: string) => {
+		closeMenu(false)
+		router.push(link)
+	}
+	const closeMenuOnClickingProductLink = () => handleClose('/products')
+	const closeMenuOnClickingContactLink = () => handleClose('/contact')
+
 	return (
 		<ul className='flex md:hidden w-full h-full z-30 mx-auto py-[80px] md:py-[100px] xl:py-[120px] flex-col justify-center absolute top-0 left-0 right-0 bottom-0 overflow-auto text-center '>
 			<motion.li
@@ -20,12 +27,7 @@ const NavMobileMenu = (props: NavMobileMenuProps) => {
 				whileInView='view'
 				transition={slideDownViewTransition(0.8)}
 				className='w-full text-[13vw] leading-[0.9em] font-heading-narrow font-[900] uppercase text-[var(--black)]'>
-				<Link
-					onClick={() => {
-						closeMenu(false)
-						router.push('/products')
-					}}
-					href='/products'>
+				<Link onClick={closeMenuOnClickingProductLink} href='/products'>
 					Products
 				</Link>
 			</motion.li>
@@ -36,12 +38,7 @@ const NavMobileMenu = (props: NavMobileMenuProps) => {
 				whileInView='view'
 				transition={slideDownViewTransition(0.9)}
 				className='w-full text-[13vw] relative leading-[0.9em] font-heading-narrow font-[900] uppercase text-[var(--black)]'>
-				<Link
-					onClick={() => {
-						closeMenu(false)
-						router.push('/contact')
-					}}
-					href='/contact'>
+				<Link onClick={closeMenuOnClickingContactLink} href='/contact'>
 					Contact
 				</Link>
 			</motion.li>
