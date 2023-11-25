a9d50857fb0ec0acb745e0e402f619343629090c
anoopraju31 <anoopraju31@gmail.com>
2023-11-25 12:39:21 +0530

    Foods - refactor

--- src/app/(home)/sections/Foods.tsx
+++ src/app/(home)/sections/Foods.tsx
@@ -1,6 +1,5 @@
 import { Button, Drooling, FoodTile } from '@/app/components'
 import { foods } from '@/utils/foodTiles'
-import React from 'react'
 
 const Foods = () => {
 	return (
@@ -8,9 +7,7 @@ const Foods = () => {
 			id='foods'
 			className='w-full px-[var(--padding-x)] md:px-[var(--padding-x-sm)] xl:px-[var(--padding-x-lg)] pt-10 lg:pt-[60px] xl:pt-[100px] z-10 relative overflow-hidden before:content-[""] before:block before:absolute before:w-[150vw] before:left-1/2 before:top-0 before:h-0 before:pb-[138%] before:-translate-x-1/2 before:bg-[var(--black)] before:rounded-[150%] before:z-0 after:content-[""] after:block after:absolute after:top-[20%] lg:after:top-[40%] after:bottom-0 after:left-0 after:right-0 after:bg-[var(--black)] after:-z-10 '>
 			{/* Heading */}
-			<div className=''>
-				<Drooling />
-			</div>
+			<Drooling />
 
 			{/* Tiles Wrapper */}
 			<div className='mb-6 lg:mb-[30px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between gap-6'>
@@ -31,6 +28,7 @@ const Foods = () => {
 					</div>
 				))}
 			</div>
+
 			{/* View More Button */}
 			<div className='mb-5 flex justify-center'>
 				<Button title='View All' link='/products' />
