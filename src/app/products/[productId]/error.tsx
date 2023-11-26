250f79af2eb2fe0ed31aa62d612b18e5c83d0c95
anoopraju31 <anoopraju31@gmail.com>
2023-11-26 12:24:19 +0530

    Product Detail Page - error page added

---
+++ src/app/products/[productId]/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const error = () => {
+	return (
+		<div className='w-full h-screen bg-[var(--black)] text-white flex justify-center items-center'>
+			404 Page Not Found.
+		</div>
+	)
+}
+
+export default error
