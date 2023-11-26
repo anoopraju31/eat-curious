8071a9b1f5b8d161983dcd1e39a3fc623e3f3a69
anoopraju31 <anoopraju31@gmail.com>
2023-11-26 10:18:25 +0530

    Contact Page - Metadata added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next'
 import { Footer } from '../components'
 import { Enquiry, FAQ, Hero } from './sections'
+
+export const metadata: Metadata = {
+	title: 'Contact - Eat Curious',
+	description:
+		'Curious to find out more? Contact us at sales@eat-curious.com and we will be in touch as soon as possible.',
+}
 
 export default function ContactPage() {
 	return (
