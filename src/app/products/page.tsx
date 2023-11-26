ff79ccbb52584d28ee6d916403ef639f0864ef93
anoopraju31 <anoopraju31@gmail.com>
2023-11-26 10:36:19 +0530

    Products Page - metadata added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next'
 import Products from './Products'
+
+export const metadata: Metadata = {
+	title: 'Product Archieve - Eat Curious',
+	description: 'Eat Curious &raquo; Products Feed',
+}
 
 const ProductPage = () => {
 	return (
